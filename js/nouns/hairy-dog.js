cd75e715e73fdee26d7f1e7d235ad204a6b61b37
Don-Pedro Ume <don-pedro.ume@yale.edu>
2026-09-23 14:06:23 -0400

    Add SOM-themed nouns from hairy-dog group

---
+++ js/nouns/hairy-dog.js
@@ -0,0 +1,14 @@
+nouns = nouns.concat([
+    'case study',
+    'cold call',
+    'spreadsheet',
+    'MBA',
+    'pitch deck',
+    'stakeholder',
+    'consultant',
+    'whiteboard',
+    'latte',
+    'networking event',
+    'bulldog',
+    'yale'
+]);
